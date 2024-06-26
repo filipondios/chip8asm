4fc36d6f08fa60e3604c7df59518776389c92844
dpv927 <dpv927@inlumine.ual.es>
2024-06-26 02:46:58 +0200

    Add until Fx07

--- src/main.zig
+++ src/main.zig
@@ -146,11 +146,7 @@ const Chip8CPU = struct {
     pub fn add_vx_vy(self: *Chip8CPU, x: u8, y: u8) void {
         const overflow = @addWithOverflow(self.regv[x], self.regv[y]);
         self.regv[x] = overflow[0];
-        if (overflow[1]) {
-            self.regv[0xf] = 1;
-        } else {
-            self.regv[0xf] = 0;
-        }
+        self.regv[0xf] = overflow[1];
     }
 
     /// Set Vx = Vx - Vy, set VF = NOT borrow.
@@ -158,14 +154,99 @@ const Chip8CPU = struct {
     /// from Vx, and the results stored in Vx.
     /// (0x8xy5)
     pub fn sub_vx_vy(self: *Chip8CPU, x: u8, y: u8) void {
-        // TODO
-        self.regv[x] = self.regv[y];
+        const overflow = @subWithOverflow(self.regv[x], self.regv[y]);
+        self.regv[x] = overflow[0];
+        self.regv[0xf] = if (overflow[1] == 1) 0 else 1;
+    }
+
+    /// Set Vx = Vx SHR 1.
+    /// If the least-significant bit of Vx is 1, then VF is set to 1,
+    /// otherwise 0. Then Vx is divided by 2.
+    /// (0x8xy6)
+    pub fn shr_vx_vy(self: *Chip8CPU, x: u8, y: u8) void {
+        self.regv[0xf] = ((0x1 & self.regv[x]));
+        self.regv[x] = @truncate(self.regv[x] >> 0x1);
+        _ = y;
+    }
+
+    /// Set Vx = Vy - Vx, set VF = NOT borrow.
+    /// If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted
+    /// from Vy, and the results stored in Vx.
+    /// (0x8xy7)
+    pub fn subn_vx_vy(self: *Chip8CPU, x: u8, y: u8) void {
+        const overflow = @subWithOverflow(self.regv[y], self.regv[x]);
+        self.regv[x] = overflow[0];
+        self.regv[0xf] = if (overflow[1] == 1) 0 else 1;
+    }
+
+    /// Set Vx = Vx SHL 1.
+    /// If the most-significant bit of Vx is 1, then VF is set to 1,
+    /// otherwise to 0. Then Vx is multiplied by 2.
+    /// (0x8xye)
+    pub fn shl_vx_vy(self: *Chip8CPU, x: u8, y: u8) void {
+        self.regv[0xf] = ((0x80 & self.regv[x]));
+        self.regv[x] = @truncate(self.regv[x] << 0x1);
+        _ = y;
+    }
+
+    /// Skip next instruction if Vx != Vy.
+    /// The values of Vx and Vy are compared, and if they are not equal,
+    /// the program counter is increased by 2.
+    /// (0x9xy0)
+    pub fn sne_vx_vy(self: *Chip8CPU, x: u8, y: u8) void {
+        if (self.regv[x] != self.regv[y]) {
+            self.pc += 2;
+        }
+    }
+
+    /// Set I = nnn.
+    /// The value of register I is set to nnn.
+    /// (0xannn)
+    pub fn ld_i_addr(self: *Chip8CPU, nnn: u16) void {
+        self.i = nnn;
+    }
+
+    /// Jump to location nnn + V0.
+    /// The program counter is set to nnn plus the value of V0.
+    /// (0xbnnn)
+    pub fn jp_v0_addr(self: *Chip8CPU, nnn: u16) void {
+        self.pc = self.regv[0x0] + nnn;
+    }
+
+    /// Set Vx = random byte AND kk.
+    /// The interpreter generates a random number from 0 to 255, which is
+    /// then ANDed with the value kk. The results are stored in Vx.
+    /// (0xcxkk)
+    pub fn rnd_vx_byte(self: *Chip8CPU, x: u8, kk: u8) void {
+        var prng = std.Random.DefaultPrng.init(undefined);
+        const rand = prng.random().int(u8);
+        self.regv[x] = rand & kk;
+    }
+
+    // TODO Dxyn - DRW Vx, Vy, nibble
+    // TODO Ex9E - SKP Vx
+    // TODO ExA1 - SKNP Vx
+
+    /// Set Vx = delay timer value.
+    /// The value of DT is placed into Vx.
+    /// (0xfx07)
+    pub fn ld_vx_dt(self: *Chip8CPU, x: u8) void {
+        self.regv[x] = self.dt;
     }
 };
 
 pub fn main() !void {
-    const a: u8 = 10; // 00001100
-    const b: u8 = 252; // 011111100
-    const overflow = @subWithOverflow(a, b);
-    std.debug.print("overflow: {}, {}", .{ overflow[0], overflow[1] });
+    // var a: u8 = 10; // 00001100
+    // const b: u8 = 10; // 011111100
+    // for (0..2) |index| {
+    //    a <<= @truncate(index);
+    // }
+
+    var prng = std.Random.DefaultPrng.init(undefined);
+    const rand = prng.random().int(u8);
+    std.debug.print("result {}", .{rand});
+
+    // if (overflow[1] == 1) {
+    //    std.debug.print("overflow", .{});
+    // } else std.debug.print("not overflow", .{});
 }
