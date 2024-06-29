7439006d8916dcf5cf6bc333c8d1fa3aba7e4016
dpv927 <dpv927@inlumine.ual.es>
2024-06-29 02:36:22 +0200

    add macros to readfile

--- tests/readfile.asm
+++ tests/readfile.asm
@@ -1,19 +1,31 @@
-
 section .data
-pathname: db "testfile",0
-fderror_msg: db "Error opening the file",0
-fdopen_msg: db "File opened",0
-buffer: times 2048 db 0
+  ;; 'Macros'
+  SYS_READ   equ 0
+  SYS_WRITE  equ 1
+  SYS_OPEN   equ 2
+  SYS_CLOSE  equ 3
+  SYS_EXIT   equ 60
+  STD_OUT    equ 1
+  S_IRUSR    equ 400
+  READ_BYTES equ 2048 
+  
+  ;; Constant string messages
+  pathname:    db "testfile",0
+  fderror_msg: db "Error opening the file",0
+  fdopen_msg:  db "File opened",0
+
+  ;; Read 2048 byte buffer
+  buffer: times READ_BYTES db 0
 
 section .text
 global _start
 _start:
   
   ;; open syscall(2)
-  mov rdi, pathname ;; file path
-  mov rsi, 00400    ;; open flag: S_IRUSR 
-  mov rax, 2
-  syscall
+  mov rdi, pathname
+  mov rsi, S_IRUSR
+  mov rax, SYS_OPEN
+  syscall 
 
   ;; error if fd < 0
   cmp rax, 0
@@ -23,10 +35,10 @@ _start:
 _fderror:
   ;; Print error
   ;; write syscall(1)
-  mov rdi, 1
+  mov rdi, STD_OUT
   mov rsi, fderror_msg
-  mov rdx, 23
-  mov rax, 1
+  mov rdx, 23 ; msg len
+  mov rax, SYS_WRITE
   syscall
   jmp _exit
 
@@ -35,26 +47,26 @@ _fdopen:
   ;; read syscall(0)
   mov rdi, rax
   mov rsi, buffer
-  mov rdx, 2048
-  mov rax, 0
+  mov rdx, READ_BYTES
+  mov rax, SYS_READ
   syscall
 
   ;; Print content
   ;; rax = byte count
   ;; rsi = buffer
   ;; write syscall(1)
-  mov rdi, 1 
+  mov rdi, STD_OUT 
   mov rdx, rax
-  mov rax, 1
+  mov rax, SYS_WRITE
   syscall
 
   ;; close syscall(3)
   mov rdi, rax
-  mov rax, 3
+  mov rax, SYS_CLOSE
   syscall
 
 _exit:
   ;; exit syscall(60)
-  mov rax, 60
+  mov rax, SYS_EXIT
   mov rdi, 0
   syscall
