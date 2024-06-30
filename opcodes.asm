11f03ea4b7217501b397464054e7cd7c3b1b988c
dpv927 <dpv927@inlumine.ual.es>
2024-06-30 02:28:07 +0200

    implement up to SHL

--- opcodes.asm
+++ opcodes.asm
@@ -1,5 +1,10 @@
-%include "memory.asm"
 extern memset
+extern cpu_v
+extern cpu_display
+extern cpu_sp
+extern cpu_pc
+extern cpu_draw
+extern cpu_stack
 
 section .text
 ;; 00E0 - CLS
@@ -128,40 +133,205 @@ global _ld_vx_byte
 _ld_vx_byte:
   mov rdx, cpu_v
   add rdx, rdi
-  mov [rdx], rsi
+  mov [rdx], byte sil
   ret
 
 
 ;; 7xkk - ADD Vx, byte
 ;; Set Vx = Vx + kk.
+;; rdi = x
+;; rsi = byte
+global _add_vx_byte;
+_add_vx_byte:
+  mov rdx, cpu_v
+  add rdx, rdi
+  mov rax, [rdx]
+  add rax, rsi
+  mov [rdx], byte al
+  ret
 
 
 ;; 8xy0 - LD Vx, Vy
 ;; Set Vx = Vy.
+;; rdi = x
+;; rsi = y
+global _ld_vx_vy:
+_ld_vx_vy:
+  mov rdx, cpu_v
+  mov rax, rdx
+  add rdx, rdi
+  add rax, rsi
+  mov rax, [rax]
+  mov rdx, byte al
+  ret
+
 
 ;; 8xy1 - OR Vx, Vy
 ;; Set Vx = Vx OR Vy.
+;; rdi = x
+;; rsi = y
+global _or_vx_xy
+_or_vx_xy:
+  mov rdx, cpu_v
+  mov rax, rdx
+  add rdx, rdi
+  add rax, rsi
+  mov rbx, [rdx]
+  mov rax, [rax]
+  or  bl, al
+  mov [rdx], byte bl
+  ret
+
 
 ;; 8xy2 - AND Vx, Vy
 ;; Set Vx = Vx AND Vy.
+;; rdi = x
+;; rsi = y
+global _and_vx_xy
+_and_vx_xy:
+  mov rdx, cpu_v
+  mov rax, rdx
+  add rdx, rdi
+  add rax, rsi
+  mov rbx, [rdx]
+  mov rax, [rax]
+  and bl, al
+  mov [rdx], byte bl 
+  ret
+
 
 ;; 8xy3 - XOR Vx, Vy
 ;; Set Vx = Vx XOR Vy.
+;; rdi = x
+;; rsi = y
+global _xor_vx_xy
+_xor_vx_xy:
+  mov rdx, cpu_v
+  mov rax, rdx
+  add rdx, rdi
+  add rax, rsi
+  mov rbx, [rdx]
+  mov rax, [rax]
+  xor bl, al
+  mov [rdx], byte bl  
+  ret
 
 ;; 8xy4 - ADD Vx, Vy
 ;; Set Vx = Vx + Vy, set VF = carry.
+;; rdi = x
+;; rsi = y
+global _add_vx_vy
+_add_vx_vy:
+  mov rdx, cpu_v
+  mov rax, rdx
+  add rdx, rdi
+  add rax, rsi
+  mov rbx, [rdx]
+  mov rax, [rax]
+  mov rcx, cpu_v
+  add rcx, 0xF
+  add bl, al
+  jnc ncarry
+  mov [rcx], 1
+  jmp end
+ncarry:
+  mov [rcx], 0
+end: 
+  mov [rdx], byte bl  
+  ret
+
 
 ;; 8xy5 - SUB Vx, Vy
 ;; Set Vx = Vx - Vy, set VF = NOT borrow.
+;; rdi = x
+;; rsi = y
+global _sub_vx_vy
+_sub_vx_vy:
+  mov rdx, cpu_v
+  mov rax, rdx
+  add rdx, rdi
+  add rax, rsi
+  mov rbx, [rdx]
+  mov rax, [rax]
+  mov rcx, cpu_v
+  add rcx, 0xF
+  sub bl, al
+  jnc ncarry
+  mov [rcx], 0
+  jmp end
+ncarry:
+  mov [rcx], 1
+end: 
+  mov [rdx], byte bl  
+  ret
+
 
 ;; 8xy6 - SHR Vx {, Vy}
 ;; Set Vx = Vx SHR 1.
+;; rdi = x
+global _shr_vx
+_shr_vx:
+  mov rsi, cpu_v
+  mov rax, rsi
+  add rax, 0xF
+  add rsi, rdi
+  mov rdx, [rsi]
+  shr dl, 1
+  jnc ncarry
+  mov [rax], 1  
+  jmp end
+ncarry:
+  mov [rax], 0
+end:
+  mov [rsi], byte dl
+  ret
+
 
 ;; 8xy7 - SUBN Vx, Vy
 ;; Set Vx = Vy - Vx, set VF = NOT borrow.
+;; rdi = x
+;; rsi = y
+global _subn_vx_vy
+_subn_vx_vy:
+  mov rdx, cpu_v
+  mov rax, rdx
+  add rdx, rdi
+  add rax, rsi
+  mov rbx, [rdx]
+  mov rax, [rax]
+  mov rcx, cpu_v
+  add rcx, 0xF
+  sub al, bl
+  jnc ncarry
+  mov [rcx], 0
+  jmp end
+ncarry:
+  mov [rcx], 1
+end: 
+  mov [rdx], byte al  
+  ret
+
 
 ;; 8xyE - SHL Vx {, Vy}
 ;; Set Vx = Vx SHL 1.
+;; rdi = x
+global _shl_vx
+_shl_vx:
+  mov rsi, cpu_v
+  mov rax, rsi
+  add rax, 0xF
+  add rsi, rdi
+  mov rdx, [rsi]
+  shl dl, 1
+  jnc ncarry
+  mov [rax], 1  
+  jmp end
+ncarry:
+  mov [rax], 0
+end:
+  mov [rsi], byte dl
+  ret
+
 
 ;; 9xy0 - SNE Vx, Vy
 ;; Skip next instruction if Vx != Vy.
